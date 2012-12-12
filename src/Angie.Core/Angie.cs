cf2bc1e52e990627831a99b0eb74a76b01edd68d
James Chambers <james@jameschambers.com>
2012-12-12 13:12:55 -0600

    moved fluent methods to partial class

--- src/Angie.Core/Angie.cs
+++ src/Angie.Core/Angie.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Angela.Core
 {
-    public class Angie
+    public partial class Angie
     {
         private static Angie _angie = new Angie();
 
@@ -110,80 +110,6 @@ namespace Angela.Core
                 default:
                     break;
             }
-        }
-
-        /// <summary>
-        /// Resets Angie to default state for next generation
-        /// and allows access to fluent interface.
-        /// </summary>
-        /// <returns></returns>
-        public static Angie Configure()
-        {
-            Reset();
-
-            return _angie;
-        }
-
-        public static void Reset()
-        {
-            _minInt = Defaults.MIN_INT;
-            _maxInt = Defaults.MAX_INT;
-            _listCount = Defaults.LIST_COUNT;
-
-            _minDateTime = DateTime.MinValue;
-            _maxDateTime = DateTime.MaxValue;
-
-            Susan.PropertyFillers.Clear();
-        }
-
-        //public Angie SomeSetterIAmComtemplating<T>(Expression<Func<T, object>> property, object value)
-        //{
-        //    // do something interesting
-        //    var propertyInfo = (((UnaryExpression)property.Body)
-        //        .Operand as MemberExpression)
-        //        .Member as PropertyInfo;
-
-        //    return _angie;
-        //}
-
-        public Angie MaxInt(int max)
-        {
-            _maxInt = max;
-            return _angie;
-            
-        }
-
-        public Angie MinInt(int min)
-        {
-            _minInt = min;
-            return _angie;
-        }
-
-        public Angie IntRange(int min, int max)
-        {
-            _minInt = min;
-            _maxInt = max;
-
-            return _angie;
-
-        }
-
-        public Angie ListCount(int count)
-        {
-            _listCount = count;
-            return _angie;
-        }
-
-        public Angie DateRange(DateTime minDateTime, DateTime maxDateTime)
-        {
-            return _angie;
-        }
-    
-        public Angie FillBy<T>(string propertyName, Func<T> filler)
-        {
-            var propName = propertyName.ToLower();
-            Susan.PropertyFillers.Add(propName, filler);
-            return _angie;
         }
 
         public static DateTime MakeDate(DateRules rules)
---
+++ src/Angie.Core/AngieFluent.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angela.Core
+{
+    public partial class Angie
+    {
+        /// <summary>
+        /// Resets Angie to default state for next generation
+        /// and allows access to fluent interface.
+        /// </summary>
+        /// <returns></returns>
+        public static Angie Configure()
+        {
+            Reset();
+
+            return _angie;
+        }
+
+        public static void Reset()
+        {
+            _minInt = Defaults.MIN_INT;
+            _maxInt = Defaults.MAX_INT;
+            _listCount = Defaults.LIST_COUNT;
+
+            _minDateTime = DateTime.MinValue;
+            _maxDateTime = DateTime.MaxValue;
+
+            Susan.PropertyFillers.Clear();
+        }
+
+        //public Angie SomeSetterIAmComtemplating<T>(Expression<Func<T, object>> property, object value)
+        //{
+        //    // do something interesting
+        //    var propertyInfo = (((UnaryExpression)property.Body)
+        //        .Operand as MemberExpression)
+        //        .Member as PropertyInfo;
+
+        //    return _angie;
+        //}
+
+        public Angie MaxInt(int max)
+        {
+            _maxInt = max;
+            return _angie;
+
+        }
+
+        public Angie MinInt(int min)
+        {
+            _minInt = min;
+            return _angie;
+        }
+
+        public Angie IntRange(int min, int max)
+        {
+            _minInt = min;
+            _maxInt = max;
+
+            return _angie;
+
+        }
+
+        public Angie ListCount(int count)
+        {
+            _listCount = count;
+            return _angie;
+        }
+
+        public Angie DateRange(DateTime minDateTime, DateTime maxDateTime)
+        {
+            return _angie;
+        }
+
+        public Angie FillBy<T>(string propertyName, Func<T> filler)
+        {
+            var propName = propertyName.ToLower();
+            Susan.PropertyFillers.Add(propName, filler);
+            return _angie;
+        }
+    }
+}
