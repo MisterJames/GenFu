cfd92a92ffee1d6e02c88a858707e247af2a1f84
James Chambers <james@jameschambers.com>
2013-01-10 08:46:05 -0600

    namespace addition
    extenstion methods hate my existence!

--- tests/ExtensionMethodTests.cs
+++ tests/ExtensionMethodTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Angela.Core.Fillers;
 
 namespace Angela.Tests
 {
@@ -24,7 +25,7 @@ namespace Angela.Tests
 
             var person = Angie.Configure<Person>()
                 .Fill(p=> p.EmailAddress)
-                //.AsEmailAddressForDomain(domain)
+                .AsEmailAddressForDomain(domain)
                 .Make<Person>();
 
             Assert.True(person.EmailAddress.Contains(domain));
