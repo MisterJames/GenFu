0474a20c1f0ffc718a9543e6176a5349e9c50432
James Chambers <james@jameschambers.com>
2014-11-06 15:28:06 -0800

    working on site

--- AngelaSmith/Angela.vNext.SampleConsole/Program.cs
+++ AngelaSmith/Angela.vNext.SampleConsole/Program.cs
@@ -7,7 +7,9 @@ namespace Angela.vNext.SampleConsole
     public class Person
     {
         public string FirstName { get; set; }
+        public string LastName { get; set; }
         public int Age { get; set; }
+        public string EmailAddress { get; set; }
 
     }
 
