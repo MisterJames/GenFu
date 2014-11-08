666ea86216cdbcf1379da22b0050335f2a9b79b0
James Chambers <james@jameschambers.com>
2014-11-07 16:21:02 -0800

    visual rev on the site

---
+++ AngelaSmith/GenFu.Web/Models/SourceCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenFu.Web.Models
+{
+    public class SourceCode
+    {
+        public string Source { get; set; }
+        
+        public bool IsLegit()
+        {
+            // todo: figure out a way to parse for mis-behaving code...
+
+            return true;
+        }
+    }
+}
