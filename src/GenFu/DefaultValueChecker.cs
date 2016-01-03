c07f6819085b38f0f0cc1a5417cd8413174a7335
Erik Schierboom <erik_schierboom@hotmail.com>
2016-01-03 12:07:27 +0100

    Replace magic strings. #54

--- src/GenFu/DefaultValueChecker.cs
+++ src/GenFu/DefaultValueChecker.cs
@@ -8,34 +8,29 @@ namespace GenFu
         internal static bool HasValue(object instance, PropertyInfo property)
         {
             var value = property.GetValue(instance,null);
-            bool valueSet = false;
-
+            
             // Support Nullable items
             if (property.PropertyType.GetTypeInfo().IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
             {
                 return value != null;
             }
 
-            switch (property.PropertyType.Name.ToLower())
+            if (property.PropertyType == typeof(int))
             {
-                case "int32":
-                    if ((Int32)value != default(Int32))
-                        valueSet = true;
-                    break;
-                case "string":
-                    if ((string)value != default(string))
-                        valueSet = true;
-                    break;
-                case "datetime":
-                    if ((DateTime)value != default(DateTime))
-                        valueSet = true;
-                    break;
-                default:
-                    break;
+                return (int)value != default(int);
             }
 
-            return valueSet;
-        }
+            if (property.PropertyType == typeof(string))
+            {
+                return (string)value != default(string);
+            }
 
+            if (property.PropertyType == typeof(DateTime))
+            {
+                return (DateTime)value != default(DateTime);
+            }
+
+            return false;
+        }
     }
 }
