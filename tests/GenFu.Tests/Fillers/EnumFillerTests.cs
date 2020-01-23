122bb214ca6de12d0222386fc0551583f62a011f
David Paquette <dave@davepaquette.com>
2020-01-22 20:40:07 -0700

    Fix bug in Enum Filler

--- tests/GenFu.Tests/Fillers/EnumFillerTests.cs
+++ tests/GenFu.Tests/Fillers/EnumFillerTests.cs
@@ -1,5 +1,7 @@
 ﻿using GenFu.Tests.TestEntities;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace GenFu.Tests.Fillers
@@ -7,11 +9,40 @@ namespace GenFu.Tests.Fillers
     public class EnumFillerTests
     {
         [Fact]
-        void Can_get_a_value()
+        public void Can_get_a_value()
         {
             var sut = new EnumFiller(typeof(BlogTypeEnum));
             var result = (BlogTypeEnum)sut.GetValue(null);
             Assert.NotNull(Enum.GetName(typeof(BlogTypeEnum), result));
+        }
+
+        [Fact]
+        public void FillsRandomlyWithAllPossibleValues()
+        {
+            var valuesNotYetUsed = new HashSet<BlogTypeEnum>();
+            var allPossibleValues = Enum.GetValues(typeof(BlogTypeEnum));
+            foreach (var value in allPossibleValues)
+            {
+                valuesNotYetUsed.Add((BlogTypeEnum)value);
+            }
+            var filler = new EnumFiller(typeof(BlogTypeEnum));
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var result = (BlogTypeEnum)filler.GetValue(null);
+                if (valuesNotYetUsed.Contains(result))
+                {
+                    valuesNotYetUsed.Remove(result);
+                    if (!valuesNotYetUsed.Any())
+                    {
+                        break;
+                    }
+                }
+            }
+
+
+            Assert.False(valuesNotYetUsed.Any(), "Enum filler did not use all possible values");
+
         }
     }
 }
