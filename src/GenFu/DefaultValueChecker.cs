59e5c82953f06fe1931549a19f1320a59395880b
XperiAndri <xperiandri@live.ru>
2022-05-10 17:09:53 +0300

    Implemented default value struct check

--- src/GenFu/DefaultValueChecker.cs
+++ src/GenFu/DefaultValueChecker.cs
@@ -5,49 +5,61 @@ using System.Reflection;
 
 namespace GenFu
 {
+    /// <summary>
+    /// Implements checking if a member has the default value
+    /// </summary>
     public class DefaultValueChecker
     {
-        internal static bool HasValue(object instance, PropertyInfo property)
+        internal static bool HasValue(object instance, MemberInfo member)
         {
-            var value = property.GetValue(instance, null);
-            if (property.PropertyType.GetTypeInfo().IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            (object value, Type type) = member switch
+            {
+                PropertyInfo property => (property.GetValue(instance, null), property.PropertyType),
+                FieldInfo field => (field.GetValue(instance), field.FieldType),
+                _ => throw new ArgumentException($"{member.Name} is not a property or field")
+            };
+
+            TypeInfo typeInfo = type.GetTypeInfo();
+            if (typeInfo.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
             {
                 return value != null;
             }
-            
-            if (value == null)
+
+            if (value is null)
                 return false;
-            if (property.PropertyType == typeof(Int32) && (Int32)value == default(Int32))
+            else if (type == typeof(Int32) && (Int32)value == default(Int32))
                 return false;
-            else if (property.PropertyType == typeof(Int16) && (Int16)value == default(Int16))
+            else if (type == typeof(Int16) && (Int16)value == default(Int16))
                 return false;
-            else if (property.PropertyType == typeof(double) && (double)value == default(double))
+            else if (type == typeof(double) && (double)value == default(double))
                 return false;
-            else if (property.PropertyType == typeof(decimal) && (decimal)value == default(decimal))
+            else if (type == typeof(decimal) && (decimal)value == default(decimal))
                 return false;
-            else if (property.PropertyType == typeof(float) && (float)value == default(float))
+            else if (type == typeof(float) && (float)value == default(float))
                 return false;
-            else if (property.PropertyType == typeof(Guid) && (Guid)value == default(Guid))
+            else if (type == typeof(Guid) && (Guid)value == default(Guid))
                 return false;
-            else if (property.PropertyType == typeof(long) && (long)value == default(long))
+            else if (type == typeof(long) && (long)value == default(long))
                 return false;
-            else if (property.PropertyType == typeof(ulong) && (ulong)value == default(ulong))
+            else if (type == typeof(ulong) && (ulong)value == default(ulong))
                 return false;
-            else if (property.PropertyType == typeof(uint) && (uint)value == default(uint))
+            else if (type == typeof(uint) && (uint)value == default(uint))
                 return false;
-            else if (property.PropertyType == typeof(ushort) && (ushort)value == default(ushort))
+            else if (type == typeof(ushort) && (ushort)value == default(ushort))
                 return false;
-            else if (property.PropertyType == typeof(bool) && (bool)value == default(bool))
+            else if (type == typeof(bool) && (bool)value == default(bool))
                 return false;
-            else if (property.PropertyType == typeof(string) && (string)value == default(string))
+            else if (type == typeof(string) && (string)value == default(string))
                 return false;
-            else if (property.PropertyType == typeof(DateTime) && ((DateTime)value).Equals(default(DateTime)))
+            else if (type == typeof(DateTime) && ((DateTime)value).Equals(default(DateTime)))
                 return false;
-            else if (property.PropertyType == typeof(DateTimeOffset) && ((DateTimeOffset)value).Equals(default(DateTimeOffset)))
+            else if (type == typeof(DateTimeOffset) && ((DateTimeOffset)value).Equals(default(DateTimeOffset)))
                 return false;
-            else if (property.PropertyType.GetTypeInfo().BaseType == typeof(System.Enum) && IsDefaultEnum(property.PropertyType, value))
+            else if (typeInfo.BaseType == typeof(System.Enum) && IsDefaultEnum(type, value))
                 return false;
-            else if (property.PropertyType.GetTypeInfo().ImplementedInterfaces.Contains(typeof(IEnumerable)) && !(value as IEnumerable).GetEnumerator().MoveNext())
+            else if (typeInfo.IsValueType && IsDefaultStruct(typeInfo, value))
+                return false;
+            else if (typeInfo.ImplementedInterfaces.Contains(typeof(IEnumerable)) && !(value as IEnumerable).GetEnumerator().MoveNext())
                 return false;
             return true;
         }
@@ -65,7 +77,10 @@ namespace GenFu
             if (t == typeof(ulong)) return ((ulong)value) == 0;
             if (t == typeof(ushort)) return ((ushort)value) == 0;
             return false;
-
         }
+
+        internal static bool IsDefaultStruct(TypeInfo typeInfo, object value)
+         => !typeInfo.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                     .Any(p => HasValue(value, p));
     }
 }
